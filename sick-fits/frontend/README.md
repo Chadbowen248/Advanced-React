30955af11f4119c4ef12cc5ef41880a46e41c607
Impactionplatform <Chadbowen248@gmail.com>
2018-10-23 19:57:32 -0400

    up to date with video

--- sick-fits/frontend/README.md
+++ sick-fits/frontend/README.md
@@ -1,3 +1,18 @@
 # Advanced-React
 
-frontend
+Frontend
+1. All Page View Components go in the pages dir.
+2. Root must be called index.js
+3. Need to wrap our views in a top level parent
+   Component, so we can share state and methods
+   to views. Next.js provides that top level
+   Component but we need to extend it so we
+   can nest the { Container } component in our
+   own mark up. See _app.js.
+        1. We'll wrap our Views in a Page Component
+           wrapper, which will have styles and markup
+           that we need on all pages, nav, footer.
+        2. Page Component will bring in a Header, Footer, Meta
+           and under Header,Meta and above Footer will call
+           {props.children} so the nested Container component
+           will populate there
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,14 +1,29 @@
+// App is the root for our pages, wraps everything
+// Extend it so that we can add state/logic and share
+// it with all of the pages
 import App, { Container } from 'next/app';
+// Page Component that we can use for style
+// and include Head and Nav
 import Page from '../components/Page';
 
 class MyApp extends App {
   render() {
+    // Get Component prop off props to render
+    // inside Page
     const { Component } = this.props;
 
     return (
       <Container>
+        {/* Page is a wrapper for our
+        different pages home, sell, whatever*/}
         <Page>
-          <Component />
+           {/* Page is bringing along other markup
+               state can go here
+           and apply to all pages. This dynamically changes
+           based on current view, *** Next Magic *** only
+           when a page is active does the next server send that
+           component/page */}
+          <Component everypage="I'll be on every one!"/>
         </Page>
       </Container>
     );
